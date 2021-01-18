ad3e5910663b1bb44b819a7498cd31e1f9f493aa
naizut <naizut@163.com>
2021-01-18 23:24:26 +0800

    mobile

---
+++ frontend/src/layout/NavMenuMobile.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="nav-menu-mobile">
+    <li v-for="route in routes"
+        :key="route.id"
+        :class="{'active': route.path === index}">
+      <router-link :to="route.path">{{route.name}}</router-link>
+    </li>
+    <a-input-search placeholder="Search title."
+                    enter-button
+                    @search="onSearch"
+                    autofocus />
+  </ul>
+</template>
