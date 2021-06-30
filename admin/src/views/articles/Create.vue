5588ba9dab40729107bb7181a8e1acd2a6c26bb4
周文恺 <naizut@163.com>
2021-06-30 11:51:28 +0800

    commit script

--- admin/src/views/articles/Create.vue
+++ admin/src/views/articles/Create.vue
@@ -2,21 +2,21 @@
     <div class="article-form">
         <a-form class="form-create-article" :form="form" @submit="publishNow">
             <a-form-item class="article-title" :span="4">
-                <a-input 
+                <a-input
                 v-decorator="['title', { rules: [{ required: true, message: '文章标题必须填写.'}]}]"
-                placeholder='文章标题'
+                placeholder='文章11标题'
                 auto-focus
                 ></a-input>
             </a-form-item>
             <tinymce ref="editor" v-model="msg" class="article-content" @onClick="onClick" />
             <a-form-item>
-                <a-input 
+                <a-input
                 v-decorator="['tag', { rules: [{ required: true, message: '文章关键词.'}]}]"
                 placeholder='文章标签'
                 ></a-input>
             </a-form-item>
             <a-form-item>
-                <a-input 
+                <a-input
                 v-decorator="['type', { rules: [{ required: true, message: '请填写文章分类.'}]}]"
                 placeholder='文章分类'
                 ></a-input>
@@ -36,10 +36,10 @@ import tinymce from '@/components/tinymce.vue'
 
 export default {
   name: 'ArticlesCreate',
-  data() {
-      return {
-          msg: ''
-      }
+  data () {
+    return {
+      msg: ''
+    }
   },
   beforeCreate () {
     this.form = this.$form.createForm(this, { name: 'publish_article' })
@@ -52,27 +52,27 @@ export default {
       console.log(this.$refs.editor.myValue)
     },
     publishNow (e) {
-        e.preventDefault()
-        
-        this.form.validateFields((err, values) => {
-            if (!err){
-                values.content = this.msg
-                values.created_on = this.getCurrentFormattedTime()
-                
-                this.axios({
-                    url: '/api/articles/create',
-                    method: 'post',
-                    data: values
-                }).then(res => {
-                    this.$router.push('/articles/list')
-                })
-                .catch(err => {
-                    console.log(err)
-                })
-            } else {
-                alert("错误格式！")
-            }
-        })
+      e.preventDefault()
+
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          values.content = this.msg
+          values.created_on = this.getCurrentFormattedTime()
+
+          this.axios({
+            url: '/api/articles/create',
+            method: 'post',
+            data: values
+          }).then(res => {
+            this.$router.push('/articles/list')
+          })
+            .catch(err => {
+              console.log(err)
+            })
+        } else {
+          alert('错误格式！')
+        }
+      })
     },
     clear () {
 	  this.$refs.editor.clear()
