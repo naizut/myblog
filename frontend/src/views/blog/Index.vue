6e085682b07147d1942e39bfca4ba99d21cb9438
naizut <naizut@163.com>
2021-08-15 23:07:17 +0800

    feat: blog filter

--- frontend/src/views/blog/Index.vue
+++ frontend/src/views/blog/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="article-list-page">
+  <div v-loading="loading" class="article-list-page">
     <div class="inner-wrap">
       <!-- BreadCrumb Here -->
       <div class="bread-crumb pd20 mb50 pl0">
@@ -8,6 +8,20 @@
 
       <!-- Banner && Brief Here -->
       <!-- Filter Here -->
+      <el-row class="article-filters pull-left mb30">
+        <div class="article-filter">
+          <el-row class="article-filter-label mb10">
+            文章分类：
+          </el-row>
+          <el-select v-model="type" @change="handleTypeSelect">
+            <el-option v-for="item in types"
+                       :key="item"
+                       :value="item"
+                       :label="item"
+            />
+          </el-select>
+        </div>
+      </el-row>
       <!-- 多个分类的前几篇文章代替filters -->
       <!-- 列表模块 -->
       <el-row class="article-list-title mb50 fadeIn">
@@ -38,30 +52,66 @@
 <script>
 export default {
   name: 'Blog',
+
   data() {
     return {
-      articles: {}
+      articles: {},
+      types: [],
+      loading: true,
+      type: this.$route.query.type || ''
     }
   },
+
   created() {
-    const that = this
-    this.axios({
-      url: '/api/articles/list',
-      method: 'get',
-      params: {
-        type: that.$route.query.type || ''
-      }
-    }).then(res => {
-      console.log(res)
-      this.articles = res.data.rows
-    })
+    this.loadPageDatas()
+  },
+
+  methods: {
+    async loadPageDatas() {
+      await this.initArticles()
+      await this.initTypes()
+    },
+
+    async initArticles() {
+      this.loading = true
+
+      await this.axios({
+        url: '/api/articles/list',
+        method: 'get',
+        params: {
+          type: this.type
+        }
+      }).then(res => {
+        console.log(res)
+        this.articles = res.data.rows
+      })
+
+      this.loading = false
+    },
+
+    async initTypes() {
+      this.axios({
+        methods: 'get',
+        url: '/api/articles/types'
+      }).then((res) => {
+        this.types = Array.from(new Set(res.data.map((x) => x.type)))
+      })
+    },
+
+    handleTypeSelect(v) {
+      this.initArticles()
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
 .article-list-page {
+  .article-filters {
+    text-align: left;
+  }
   .article-list-title {
     text-align: left;
+    clear: both;
   }
   .article-list {
     border-left: 2px solid #f2f2f2;
